fa6e499c99fc0ace18998a1bfe3f7d2cb8cf5104
Surterre <mirandahidalgo21@gmail.com>
2019-11-23 21:49:52 -0500

    switch case created to call the functions

--- liriapp.js
+++ liriapp.js
@@ -9,3 +9,20 @@ var axios = require('axios');
 var fs = require('fs'); 
 var command = process.argv[2]; 
 var value = process.argv[3]; 
+
+
+switch (command) {
+    case "concert-this":
+        concertThis(value);
+        break;
+    case "spotify-this-song":
+        spotifySong(value);
+        break;
+    case "movie-this":
+        movieThis(value);
+        break;
+    case "do-what-it-says":
+        doThis(value);
+        break;
+};
+
