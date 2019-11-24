002553083034e8a358918eddb4b750e17b368683
Surterre <mirandahidalgo21@gmail.com>
2019-11-23 21:47:50 -0500

    created variables for liriapp

--- liriapp.js
+++ liriapp.js
@@ -1,8 +1,11 @@
-const axios = require("axios");
+require("dotenv").config();
 
-axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=11165f40").then(
-    function(response) {
-        console.log("the movie rating is: "+ response.data.imdbRating)
-    }
-);
-
+var keys = require(".keys.js");
+var Spotify = require('node-spotify-api'); 
+var spotify = new Spotify(keys.spotify);
+var moment = require('moment'); 
+moment().format();
+var axios = require('axios'); 
+var fs = require('fs'); 
+var command = process.argv[2]; 
+var value = process.argv[3]; 
