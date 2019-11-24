d23f1885b82f412a7f403537375079c5aac0d113
Surterre <mirandahidalgo21@gmail.com>
2019-11-23 21:53:40 -0500

    concert this function added, tried to node it but not working yet

--- liriapp.js
+++ liriapp.js
@@ -26,3 +26,30 @@ switch (command) {
         break;
 };
 
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
+
+            var datetime = response.data[i].datetime; 
+            var dateArr = datetime.split('T'); 
+
+            var concertResults = 
+                "--------------------------------------------------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name + 
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); 
+            console.log(concertResults);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+        
+
+}
+
+
+
+
+
